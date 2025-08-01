6004665652c6b78b4fb019d31cebf34be0b97cf8
Amaresh Pelleti <amareswer@hotmail.com>
2025-08-01 17:21:53 -0400

    refactor: remove QuickBarcodePrinter component and clean up imports in BarcodeClientWrapper

--- src/components/admin/BarcodeClientWrapper.tsx
+++ src/components/admin/BarcodeClientWrapper.tsx
@@ -4,7 +4,6 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-// import QuickBarcodePrinter from './QuickBarcodePrinter'
 import WorkingBarcodePrinter from './WorkingBarcodePrinter'
 
 interface BarcodeClientWrapperProps {
